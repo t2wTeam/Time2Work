b99a9b6425b8f15a59b64d55a34b769610821dd6
lkt <lkt@temp.com>
2024-01-02 01:53:13 -0500

    updated border color

--- client/src/Items/TimeFragment.tsx
+++ client/src/Items/TimeFragment.tsx
@@ -7,17 +7,27 @@ interface TimeFragment {
 }
 
 export const TimeFragment = (props: TimeFragment) => {
-    let { fragments } = props
+    let { fragments } = props;
     return (
         <Grid container width="100%" height="100%">
-            {
-                fragments.map((availibility, i) => (
-                    <Grid item key={i} xs={3} sx={{ bgcolor: availibility ? "#4F7942" : "#F2F3F5", border:0.5, borderStyle:"dotted" }} width="25%" height="100%"/>
-                ))
-            }
+            {fragments.map((availibility, i) => (
+                <Grid
+                    item
+                    key={i}
+                    xs={3}
+                    sx={{
+                        bgcolor: availibility ? "#4F7942" : "#F2F3F5",
+                        border: availibility ? 0 : 0.5, // Apply border only when availibility is false
+                        borderStyle: "dotted",
+                        borderColor: "#DDDDDD", // Border color
+                    }}
+                    width="25%"
+                    height="100%"
+                />
+            ))}
         </Grid>
-    )
-}
+    );
+};
 
 
 export const TimeFragmentVertical = (props: TimeFragment) =>{
@@ -26,10 +36,11 @@ export const TimeFragmentVertical = (props: TimeFragment) =>{
         <Grid container width="100%" height="100%">
             {
                 fragments.map((availibility, i) => (
-                    <Grid item key={i} xs={12}  width="100%"  height="25%"  
+                    <Grid item key={i} xs={12}  width="100%"  height="25%"
                     sx={{ 
                         bgcolor: availibility ? "#4F7942" : "#F2F3F5",
                         borderTop: i === 2 ?  "1px dashed": "none",
+                        borderColor: "#DDDDDD",
                     }}/>
                 ))
             }
