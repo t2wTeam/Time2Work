40e715b4c0dadf916ec4c3be6eed7f7e5f7dc48f
lkt <lkt@temp.com>
2024-01-03 00:06:59 -0500

    updated border line

--- client/src/Items/TimeFragment.tsx
+++ client/src/Items/TimeFragment.tsx
@@ -40,10 +40,10 @@ export const TimeFragmentVertical = (props: TimeFragment) =>{
                     <Grid item key={i} xs={12}  width="100%"  height="25%"
                     sx={{ 
                         bgcolor: availibility ? "#4F7942" : "transparent",
-                        border: availibility ? 0 : 0.25, // Apply border only when availibility is false
-                        // borderTop: i === 2 ?  "0.5px dashed": "none",
-                        borderColor: "#DDDDDD",
-                        borderStyle: "dotted",
+                        // border: availibility ? 0 : 0.25, // Apply border only when availibility is false
+                        borderTop: i === 2 ?  "0.5px dashed": "none",
+                        // borderColor: "#DDDDDD",
+                        // borderStyle: "dotted",
                     }}/>
                 ))
             }
